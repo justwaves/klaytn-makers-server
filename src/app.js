8a055936b5079345b75d443ac0f633a5dd801ae9
uijin <justwaves@naver.com>
2020-05-22 15:46:34 +0900

    Koa static

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@ import Koa from "koa";
 import Router from "koa-router";
 import bodyParser from "koa-bodyparser";
 import mongoose from "mongoose";
+import serve from "koa-static";
+import path from "path";
+import send from "koa-send";
+
 import api from "./api";
 import jwtMiddleware from "./lib/jwtMiddleware";
 // import createFakeData from "./lib/createFakeData";
@@ -30,6 +34,17 @@ app.use(jwtMiddleware);
 
 // app 인스턴스에 router 적용
 app.use(router.routes()).use(router.allowedMethods());
+
+const buildDirectory = path.resolve(
+  __dirname,
+  "../../klaytn-makers-client/build",
+);
+app.use(serve(buildDirectory));
+app.use(async ctx => {
+  if (ctx.status === 404 && ctx.path.indexOf("/api") !== 0) {
+    await send(ctx, "index.html", { root: buildDirectory });
+  }
+});
 
 const port = PORT || 4000;
 app.listen(port, () => {
