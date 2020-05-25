19dbca92b5b44d81eb0614833d15bb1b5c08b5cc
uijin <justwaves@naver.com>
2020-05-25 15:40:05 +0900

    request

--- src/app.js
+++ src/app.js
@@ -56,6 +56,14 @@ app.use(async ctx => {
   }
 });
 
+app.all("/*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "POST, PUT, GET, DELETE");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
+
 const port = PORT || 4000;
 app.listen(port, () => {
   console.log(`Listening to port ${port}`);
